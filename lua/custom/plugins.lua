6eba7fb62988dec9d508fd5e193ce3e4f088bede
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-02-13 17:13:25 -0300

    Add Copilot config for NVChad.

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -5,6 +5,17 @@ local plugins = {
 
   -- Override plugin definition options
 
+    {
+    "zbirenbaum/copilot.lua",
+    -- Lazy load when event occurs. Events are triggered
+    -- as mentioned in:
+    -- https://vi.stackexchange.com/a/4495/20389
+    event = "InsertEnter",
+    -- You can also have it load at immediately at
+    -- startup by commenting above and uncommenting below:
+    -- lazy = false
+    opts = overrides.copilot,
+  },
   {
     "neovim/nvim-lspconfig",
     config = function()
