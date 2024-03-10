6d9a08f2a548e7de63ab0f3cd8d76ddf44e450c7
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2024-03-10 19:01:59 -0300

    Update lazy.nvim framework configuration.

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,25 +1,32 @@
 return {
+  {
+    -- "github/copilot.vim",
+    "nvim-lua/plenary.nvim",
+    "ellisonleao/gruvbox.nvim",
+    { "mg979/vim-visual-multi", branch = "master" },
+    "tpope/vim-fugitive",
     {
-        "github/copilot.vim",
+      "lewis6991/gitsigns.nvim",
+      config = function()
+        require("gitsigns").setup({
+          current_line_blame = true,
+          current_line_blame_formatter = "<author> • <author_time:%d/%m/%Y %H:%M> • <summary>",
+        })
+      end,
+    },
+    {
+      "kdheepak/lazygit.nvim",
+      -- optional for floating window border decoration
+      dependencies = {
         "nvim-lua/plenary.nvim",
-        "ellisonleao/gruvbox.nvim",
-        { "mg979/vim-visual-multi", branch = "master" },
-        "tpope/vim-fugitive",
-        {
-            "lewis6991/gitsigns.nvim",
-            config = function()
-                require('gitsigns').setup {
-                    current_line_blame = true,
-					current_line_blame_formatter = '<author> • <author_time:%d/%m/%Y %H:%M> • <summary>',
-                }
-            end
-        },
-        {
-            "kdheepak/lazygit.nvim",
-            -- optional for floating window border decoration
-            dependencies = {
-                "nvim-lua/plenary.nvim",
-            },
-        },
-    }
+      },
+    },
+    {
+      "numToStr/Comment.nvim",
+      opts = {
+        -- add any options here
+      },
+      lazy = false,
+    },
+  },
 }
